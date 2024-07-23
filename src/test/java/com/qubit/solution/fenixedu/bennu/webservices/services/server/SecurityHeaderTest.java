0747c64adc99a5a1264a625042a78527ce7d3fbf
João Freitas <joao.freitas@qub-it.com>
2024-07-23 13:11:58 +0100

    Removes usages of junit 5
    Relates with: #qubIT-Omnis-6262

--- src/test/java/com/qubit/solution/fenixedu/bennu/webservices/services/server/SecurityHeaderTest.java
+++ src/test/java/com/qubit/solution/fenixedu/bennu/webservices/services/server/SecurityHeaderTest.java
@@ -6,21 +6,19 @@ import javax.crypto.NoSuchPaddingException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
 
 public class SecurityHeaderTest {
 
     @Test
     public void testGetPrivateKeyCipherInstanceExists()
             throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException {
-        assertDoesNotThrow(SecurityHeader::getPrivateKeyCipherInstance);
         assertNotNull(SecurityHeader.getPrivateKeyCipherInstance());
     }
 
     @Test
     public void testGetSessionCipherInstanceExists()
             throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException {
-        assertDoesNotThrow(SecurityHeader::getSessionCipherInstance);
         assertNotNull(SecurityHeader.getSessionCipherInstance());
     }
 
